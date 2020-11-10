3cd895a5fba587a594e5ef290d5f15172aad0488
Pascal Geiser <pgeiser@pgeiser.com>
2020-11-10 20:08:57 +0100

    Update configure.sh to use local bash-helpers folder.

--- configure.sh
+++ configure.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-set -eu
-
-# Fetch bash helpers and source it.
-if [ ! -e .bash_helpers.sh ]; then
-  curl -kSL https://raw.githubusercontent.com/13pgeiser/bash/master/bash_helpers.sh -o .bash_helpers.sh
-fi
-source ./.bash_helpers.sh
-
+set -ex
+source bash-helpers/helpers.sh
 # Get submodules
 git submodule update --init
-
 # Install required tools
-_detect_tools_folder
-_install_buildessentials
-_install_cmake
-_install_gcc_arm_none_eabi
-_install_subversion
-_write_sourceme
+#_detect_tools_folder
+install_buildessentials
+install_cmake
+install_gcc_arm_none_eabi
+#_install_subversion
+write_sourceme
 
 # Done! Now call cmake
-_call_cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake
+call_cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake
