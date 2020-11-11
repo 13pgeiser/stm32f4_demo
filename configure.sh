469862d9f772e576e3cffd005427c6f6b179d497
Pascal Geiser <pgeiser@pgeiser.com>
2020-11-11 17:46:53 +0100

    Install svn client.

--- configure.sh
+++ configure.sh
@@ -4,12 +4,11 @@ source bash-helpers/helpers.sh
 # Get submodules
 git submodule update --init
 # Install required tools
-#_detect_tools_folder
 install_buildessentials
+install_subversion
 install_cmake
 install_gcc_arm_none_eabi
 #_install_subversion
 write_sourceme
-
 # Done! Now call cmake
 call_cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake
