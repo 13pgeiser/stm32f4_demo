9a14cd176904fb4dc56c067b08934edbce70609b
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2022-11-20 16:07:13 +0100

    Use shared submodule

--- configure.sh
+++ configure.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-set -ex
-source bash-helpers/helpers.sh
-run_shfmt_and_shellcheck
+source bash-scripts/helpers.sh
+run_shfmt_and_shellcheck ./*.sh
 # Get submodules
 git submodule update --init
 # Install required tools
-install_buildessentials
-install_subversion
+install_packages make ninja subversion
 install_cmake
 install_gcc_arm_none_eabi
-#_install_subversion
 write_sourceme
 # Done! Now call cmake
 call_cmake -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi.cmake
